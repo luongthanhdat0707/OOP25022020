8e3756445bc0a659e80e6977a40bf2de7792c988
luongthanhdat0707 <luongthanhdat0707>
2020-04-09 15:29:58 +0700

    Object and class

--- app/src/main/java/com/example/opp25022020/MainActivity.java
+++ app/src/main/java/com/example/opp25022020/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.opp25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Object và class
+        //Object là một đối tượng, thông tin cụ thể, cá thể có đủ thông tin
+        //Class là một đối tượng tượng trưng cho một tập thể, là một kiểu dữ liệu được tạo ra trong một tập thể và để xem tập thể đó có gì giống với nhau
+        Person Luongthanhdat = new Person();
+        Luongthanhdat.name="Luong Thanh Dat";
+        Luongthanhdat.age = 10 ;
+        Log.d("BBB",Luongthanhdat.name + " " + Luongthanhdat.age);
+
+
     }
 }
---
+++ app/src/main/java/com/example/opp25022020/Person.java
@@ -0,0 +1,10 @@
+package com.example.opp25022020;
+
+public class Person {
+    //chung thuộc tính và hành vi với nhau
+    //Thuộc tính
+    String name;
+    int age;
+
+    //Hành vi
+}
